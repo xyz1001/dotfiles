1f59a8ca3a37d8c2b88c61a81d0881a598ba2542
xyz1001 <zgzf1001@gmail.com>
2021-07-09 15:37:41 +0800

    修复cdbuild在build目录存在时执行不正常的bug

--- 4c761f170e016836ff84498202b99827/.zsh/function.zsh
+++ 4c761f170e016836ff84498202b99827/.zsh/function.zsh
@@ -4,4 +4,4 @@ function gi() { curl -sLw n https://www.gitignore.io/api/$@ | tee -a .gitignore;
 
 function caps() { setxkbmap -option ctrl:nocaps; xcape -e 'Control_L=Escape' }
 
-function cdbuild() { if [ -d \"./build\" ]; then cd build; else mkdir build && cd build; fi }
+function cdbuild() { if [ -d "./build" ]; then cd build; else mkdir build && cd build; fi }
