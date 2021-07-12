dcb832dfc2314f1ef3cb42663ad6cc3e657e4485
xyz1001 <zgzf1001@gmail.com>
2021-07-12 11:47:52 +0800

    移除重复的配置

--- 4c761f170e016836ff84498202b99827/.zsh/config.zsh
+++ 4c761f170e016836ff84498202b99827/.zsh/config.zsh
@@ -47,7 +47,3 @@ fi
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" --no-use  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-if [[ "$(uname 2> /dev/null)" == "Darwin"* ]]; then
-    export PATH="/usr/local/opt/llvm/bin":$PATH
-fi
