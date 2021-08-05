89d7b4b5a30e54652fbb1ee012518066f3f59a73
xyz1001 <zgzf1001@gmail.com>
2021-08-05 16:27:14 +0800

    修改wt配色

--- 109dd9c58466739d9d846be552d645fc/Microsoft.PowerShell_profile.ps1
+++ 109dd9c58466739d9d846be552d645fc/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-Import-Module "Oh-My-Posh" -DisableNameChecking -NoClobber
+Import-Module Oh-My-Posh -DisableNameChecking -NoClobber
 Import-Module posh-git
 Import-Module PSReadLine
 Import-Module git-aliases -DisableNameChecking
