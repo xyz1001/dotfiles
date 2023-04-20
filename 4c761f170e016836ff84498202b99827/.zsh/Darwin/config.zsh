bab4ff417fed0c397930af5679e5a78177a5283d
xyz1001 <zgzf1001@gmail.com>
2023-04-20 17:54:21 +0800

    macOS下添加Qt5_DIR环境变量

--- 4c761f170e016836ff84498202b99827/.zsh/Darwin/config.zsh
+++ 4c761f170e016836ff84498202b99827/.zsh/Darwin/config.zsh
@@ -4,6 +4,7 @@
 
 export ANDROID_HOME=$HOME/Library/Android/sdk
 export ANDROID_NDK_HOME=$HOME/Library/Android/sdk/ndk/16.1.4479499
+export Qt5_DIR=/opt/homebrew/opt/qt5
 
 export PATH="/usr/local/opt/llvm/bin":$PATH
 
