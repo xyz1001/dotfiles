ca1dd578a654c4e4eef7fccf3714aef82e8d9c09
xyz1001 <zgzf1001@gmail.com>
2025-04-09 11:27:12 +0800

    添加barbar.nvim插件

--- 62b5a4f5584787034192bed3d4bb011d/nvim/lua/config/plugins/common.lua
+++ 62b5a4f5584787034192bed3d4bb011d/nvim/lua/config/plugins/common.lua
@@ -274,4 +274,16 @@ return {
 			require("im_select").setup({})
 		end,
 	},
+	{
+		"romgrk/barbar.nvim",
+		dependencies = {
+			"lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
+			"nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
+		},
+		init = function()
+			vim.g.barbar_auto_setup = false
+		end,
+		opts = {},
+		version = "^1.0.0", -- optional: only update when a new 1.x version is released
+	},
 }
