337d2805cda60dc8799e3a2867c097225d8affbb
xyz1001 <zgzf1001@gmail.com>
2025-04-13 12:51:47 +0800

    添加im-select的安装

--- 62b5a4f5584787034192bed3d4bb011d/nvim/lua/config/plugins/common.lua
+++ 62b5a4f5584787034192bed3d4bb011d/nvim/lua/config/plugins/common.lua
@@ -290,6 +290,15 @@ return {
 	},
 	{
 		"keaising/im-select.nvim",
+		build = function()
+			if vim.fn.has("win32") == 1 then
+				vim.fn.system("scoop install im-select")
+			elseif vim.fn.has("wsl") == 1 then
+				vim.fn.system(
+					"curl -o ~/.local/bin/im-select.exe https://github.com/daipeihust/im-select/raw/1.0.1/im-select-win/out/x64/im-select.exe && chmod +x ~/.local/bin/im-select.exe"
+				)
+			end
+		end,
 		config = function()
 			require("im_select").setup({})
 		end,
