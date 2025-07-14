21dd1a0ab745378140b7d8e792aa894d355b3ff8
xyz1001 <zgzf1001@gmail.com>
2025-07-14 10:03:36 +0800

    修复avante配置接口变动警告

--- 62b5a4f5584787034192bed3d4bb011d/nvim/lua/config/plugins/ai.lua
+++ 62b5a4f5584787034192bed3d4bb011d/nvim/lua/config/plugins/ai.lua
@@ -72,14 +72,18 @@ return {
 		event = "VeryLazy",
 		opts = {
 			provider = "copilot",
-			copilot = {
-				endpoint = "https://api.githubcopilot.com",
-				model = "claude-3.5-sonnet",
-				proxy = nil, -- [protocol://]host[:port] Use this proxy
-				allow_insecure = false, -- Allow insecure server connections
-				timeout = 30000, -- Timeout in milliseconds
-				temperature = 0,
-				max_tokens = 20480,
+			providers = {
+				copilot = {
+					endpoint = "https://api.githubcopilot.com",
+					model = "claude-3.5-sonnet",
+					proxy = nil, -- [protocol://]host[:port] Use this proxy
+					allow_insecure = false, -- Allow insecure server connections
+					timeout = 30000, -- Timeout in milliseconds
+					extra_request_body = {
+						temperature = 0,
+						max_tokens = 20480,
+					},
+				},
 			},
 			behaviour = {
 				auto_suggestions = false,
