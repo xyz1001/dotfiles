edac657d4ae773442719a395c72d149527161861
xyz1001 <zgzf1001@gmail.com>
2025-09-23 15:12:57 +0800

    移除Powershell 5.1的插件配置

--- 109dd9c58466739d9d846be552d645fc/Microsoft.PowerShell_profile.ps1
+++ 109dd9c58466739d9d846be552d645fc/Microsoft.PowerShell_profile.ps1
@@ -1,16 +1,1 @@
-Import-Module Oh-My-Posh -DisableNameChecking -NoClobber
-Import-Module posh-git
-Import-Module PSReadLine
-Import-Module git-aliases -DisableNameChecking
-Import-Module VSSetup
-Import-Module Pscx
-
-Set-PSReadLineOption -EditMode vi
-Set-PSReadLineOption -PredictionSource History
-Set-PSReadLineKeyHandler -Chord Ctrl+p -Function HistorySearchBackward
-Set-PSReadLineKeyHandler -Chord Ctrl+n -Function HistorySearchForward
-Set-PSReadLineKeyHandler -Chord Tab -Function MenuComplete
-Set-PSReadLineKeyHandler -Chord Ctrl+o -Function ClearScreen
-Set-PSReadLineKeyHandler -Chord Ctrl+w BackwardDeleteWord
-Set-Theme robbyrussell
 $Env:LESSCHARSET="utf-8"
