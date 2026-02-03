c25076f176993d26940432e4e104db72fa1931ac
xyz1001 <zgzf1001@gmail.com>
2026-02-03 10:49:06 +0800

    修复C-k无法向上切换窗口的问题

--- 62b5a4f5584787034192bed3d4bb011d/nvim/lua/config/plugins/develop.lua
+++ 62b5a4f5584787034192bed3d4bb011d/nvim/lua/config/plugins/develop.lua
@@ -148,7 +148,6 @@ return {
 					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
 					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-					vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
 					vim.keymap.set("n", "<leader>x", quickfix, opts)
 					vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
 					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
